6c10a0f1ffc85ddc1e7f1aa2af7c5f5d07957d00
matthias <h1395010@connect.hku.hk>
2015-02-13 12:43:02 +0800

    reformatted, removed primary radical count, simplied

--- text_processing.java
+++ text_processing.java
@@ -9,23 +9,19 @@ public class text_processing
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws IOException
 	{
-		//LinkedList<String> output_list = new LinkedList<String>();
 		
 		String text; 
 		BufferedReader br = new BufferedReader(new FileReader("/home/matthias/Workbench/SUTD/1_February/brute_force/brutus_items.csv"));
 
 		while ((text = br.readLine()) != null) 
 		{
-			//container
-			//LinkedList<String> output_container = new LinkedList<String>();
 			
 			//the main character
 			Pattern pat_0 = Pattern.compile( "『(.*?)』" );
 			Matcher mat_0 = pat_0.matcher( text );
 			if( mat_0.find() )
 			{
-			    System.out.println( mat_0.group(1) );
-			    //output_container.add( mat_0.group(1) );
+			    System.out.println("\n" + mat_0.group(1) );
 			    
 			}
 			//the pin yin
@@ -34,7 +30,6 @@ public class text_processing
 			if( mat_1.find() )
 			{
 			    System.out.println( mat_1.group(1) );
-			    //output_container.add( mat_1.group(1) );
 			}
 			//the ubiquitous radical 
 			Pattern pat_2 = Pattern.compile( "<span class=\"\"b\"\">部首：</span>" ); 
@@ -46,54 +41,25 @@ public class text_processing
 			    if( mat_3.find() )
 				{
 			    	System.out.println("部首：" + mat_3.group(1) );
-			    	//output_container.add( mat_3.group(1) );
 				}
 				//stroke count
-				Pattern pat_4 = Pattern.compile(mat_3.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
-				Matcher mat_4 = pat_4.matcher( text );
-			    if( mat_4.find() )
-				{
-			    	System.out.println("笔画：" + mat_4.group(1) );
-			    	//output_container.add( mat_4.group(1) );
-				}
+				//Pattern pat_4 = Pattern.compile(mat_3.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
+				//Matcher mat_4 = pat_4.matcher( text );
+			    //if( mat_4.find() )
+				//{
+			    //	System.out.println("笔画：" + mat_4.group(1) );
+				//}
 
 			}
 			else
 			{
-				//simple rad
+				//simple rad (简体)
 				Pattern pat_5 = Pattern.compile("简体部首：</span>(.*?)<span class=\"\"b\"\">");
 			    Matcher mat_5 = pat_5.matcher( text );
 			    if( mat_5.find() )
 				{
-			    	System.out.println("简体部首：" + mat_5.group(1) );
-			    	//output_container.add( mat_5.group(1) );
+			    	System.out.println("部首：" + mat_5.group(1) );
 				
-			    	//stroke count
-			    	Pattern pat_6 = Pattern.compile(mat_5.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
-			    	Matcher mat_6 = pat_6.matcher( text );
-			    	if( mat_6.find() )
-			    	{
-			    		System.out.println("简体笔画：" + mat_6.group(1) );
-			    		//output_container.add( mat_6.group(1) );
-			    	}
-				}
-			    
-			    //trad rad
-				Pattern pat_7 = Pattern.compile("繁体部首：</span>(.*?)<span class=\"\"b\"\">");
-			    Matcher mat_7 = pat_7.matcher( text );
-			    if( mat_7.find() )
-				{
-			    	System.out.println("繁体部首：" + mat_7.group(1) );
-			    	//output_container.add( mat_7.group(1) );
-				
-			    	//stroke count
-			    	Pattern pat_8 = Pattern.compile(mat_7.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
-			    	Matcher mat_8 = pat_8.matcher( text );
-			    	if( mat_8.find() )
-			    	{
-			    		System.out.println("繁体笔画：" + mat_8.group(1) );
-			    		//output_container.add( mat_8.group(1) );
-			    	}
 				}
 			    
 
@@ -105,17 +71,42 @@ public class text_processing
 			if( mat_9.find() )
 			{
 			    System.out.println("首尾分解: " + mat_9.group(1) );
-			    //output_container.add( mat_9.group(1) );
 			}
-			
-			
-			
+
 		}
 		
 		
 	}
 }
 
+
+			    /*
+			    trad rad
+				Pattern pat_7 = Pattern.compile("繁体部首：</span>(.*?)<span class=\"\"b\"\">");
+			    Matcher mat_7 = pat_7.matcher( text );
+			    if( mat_7.find() )
+				{
+			    	System.out.println("繁体部首：" + mat_7.group(1) );
+				
+			    	//stroke count
+			    	Pattern pat_8 = Pattern.compile(mat_7.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
+			    	Matcher mat_8 = pat_8.matcher( text );
+			    	if( mat_8.find() )
+			    	{
+			    		System.out.println("繁体笔画：" + mat_8.group(1) );
+			    	}
+				}
+				*/
+
+
+
+			    	//stroke count
+			    	//Pattern pat_6 = Pattern.compile(mat_5.group(1) + "<span class=\"\"b\"\">部首笔画：</span>(.*?)<span class=\"\"b\"\">");
+			    	//Matcher mat_6 = pat_6.matcher( text );
+			    	//if( mat_6.find() )
+			    	//{
+			    	//	System.out.println("简体笔画：" + mat_6.group(1) );
+			    	//}
 
     // #######################," ]：占乂(zhancha)
     // ","<table width=""620"" border=""0"" cellpadding=""0"" cellspacing=""0"">
